0515cbb6e246bf812ff06d49407e646b5ad9d234
Ryoichi Sugiura <2757226+whale-shark@users.noreply.github.com>
2024-06-12 09:40:30 +0900

    Initial commit.

---
+++ src/main/java/com/example/practicemavenrenovate/PracticeMavenRenovateApplication.java
@@ -0,0 +1,13 @@
+package com.example.practicemavenrenovate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMavenRenovateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeMavenRenovateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practicemavenrenovate/PracticeMavenRenovateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicemavenrenovate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMavenRenovateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
